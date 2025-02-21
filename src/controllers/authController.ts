c9520bc285cb917a591eb08e9fadcfdf2bd696c2
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-02-21 19:34:41 +0100

    feat: :white_check_mark: controller and authService done

---
+++ src/controllers/authController.ts
@@ -0,0 +1,41 @@
+// Manages HTTP requests related to users.
+// Contains methods for handling routes like GET, POST, PUT, DELETE for users.
+// Delegates business logic to the user service.
+
+import { NextFunction, type Request, type Response } from 'express';
+import { AuthService } from '../services/auth.service';
+
+export class AuthController {
+  private authService: AuthService;
+
+  constructor() {
+    this.authService = new AuthService();
+  }
+  login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const tokenData = await this.authService.login(req.body.email, req.body.password);
+      const response = {
+        message: 'User successfully logged in',
+        accessToken: tokenData.accessToken,
+        refreshToken: tokenData.refreshToken,
+      };
+      res.send(response);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const registeredUser = await this.authService.register(req.body);
+      const response = {
+        message: `${registeredUser?.name} successfully registered`,
+        user: registeredUser
+      };
+      res.send(response);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+}
